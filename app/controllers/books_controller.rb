8ead3cf2dc9eefc0ae993ff81bff5807ee76fb02
mmch0626 <office_hagi@yahoo.co.jp>
2019-09-16 05:50:55 +0000

    [Finish 4-8]

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,82 @@
+class BooksController < ApplicationController
+  def index
+    @books = Book.all
+    # 追加部分
+    @book = Book.new
+  end
+
+
+
+
+  def show
+
+    @book = Book.find(params[:id])
+  end
+
+
+  def create
+    @book = Book.new(book_params)
+    @books = Book.all
+    # book.save
+# 追加部分
+
+if @book.save
+    flash[:notice] = "Book was successfully created."
+    redirect_to book_path(@book.id)
+  else
+    render 'index'
+  end
+
+# ここまで
+
+  end
+
+
+
+
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+
+  # def update
+  #   book = Book.find(params[:id])
+  #   book.update(book_params)
+  #   redirect_to book_path(book)
+
+  # end
+
+    def update
+     @book = Book.find(params[:id])
+  if @book.update(book_params)
+    flash[:notice] = "Book was successfully updated."
+    redirect_to book_path(@book.id)
+  else
+    render action: :edit
+  end
+end
+
+
+
+
+
+
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+
+
+    flash[:notice] = "Book was successfully destroyed."
+  end
+
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+end
---
+++ app/models/book.rb
@@ -0,0 +1,9 @@
+class Book < ApplicationRecord
+
+
+
+	validates :title,:body, presence: true
+
+
+
+end
